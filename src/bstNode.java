7e6e0993df1950759723e04f6a083af353017558
Pantherinae <blade_master_pantherinae@hotmail.com>
2018-09-12 11:44:22 -0600

    Initial Backup

---
+++ src/bstNode.java
@@ -0,0 +1,22 @@
+
+public class bstNode
+{
+
+	public int item;
+	public bstNode left;
+	public bstNode right;
+
+	public bstNode(int i)
+	{
+		item = i;
+		left = null;
+		right = null;
+	}
+
+	public bstNode(int i, bstNode l, bstNode r)
+	{
+		item = i;
+		left = l;
+		right = r;
+	}
+}
